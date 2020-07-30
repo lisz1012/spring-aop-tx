ffc5a656be905c1cb02d37645872a3945e23aa47
lisz1012 <lisz1012@163.com>
2020-07-29 22:52:54 -0700

    配置声明式的事务管理（测试通过，一旦有异常所有的写操作的效果都会被取消）

--- src/main/java/com/lisz/service/BookService.java
+++ src/main/java/com/lisz/service/BookService.java
@@ -3,12 +3,14 @@ package com.lisz.service;
 import com.lisz.dao.BookDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookService {
 	@Autowired
 	private BookDao bookDao;
 
+	@Transactional
 	public void buyBook(int userId, int bookId, int quantity) {
 		double bookPrice = bookDao.getPriceByBookId(bookId);
 		double balance = bookDao.getBalanceByUserId(userId);
@@ -17,6 +19,8 @@ public class BookService {
 		if (instockQuantity < quantity) throw new IllegalArgumentException(("Not enough instock quantity"));
 		balance -= bookPrice * quantity;
 		bookDao.updateBalance(userId, balance);
+		// 在两个写操作之间抛出异常
+		int i = 1/0;
 		bookDao.updateStock(bookId, quantity);
 		System.out.println(String.format("User %s just bought %s books with book ID: %s",
 				userId, quantity, bookId));
